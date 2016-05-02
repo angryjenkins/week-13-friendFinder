668b73a0b3df023a7e8ccaca98cd30d5a388b255
m. nathaniel martin <matt.martin@rutgers.edu>
2016-05-01 20:18:58 -0400

    data routing seems to work, need to add friend comparison logic and make it display in modal

--- app/data/survey-data.js
+++ app/data/survey-data.js
@@ -1,3 +1,9 @@
-var friendsArray = [];
+var friendsArray = [
+	{
+		name: 'Matt',
+		pic:'https://avatars2.githubusercontent.com/u/14875183?v=3&s=460',
+		score: '35'
+	}
+];
 
 module.exports = friendsArray;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -3,20 +3,28 @@
 //     A GET route with the url /api/friends. This will be used to display a JSON of all possible friends
 //     A POST routes /api/friends. This will be used to handle incoming survey results. This route will also be used to handle the compatibility logic.
 
-
-var friendsData = require('../data/survey-data.js');
+var friendsArray = require('../data/survey-data.js');
+var path = require('path');
 
 module.exports = function(app){
 
 	app.get('/api/friends', function(req, res){
-	    res.json(friendsData);
+	    res.json(friendsArray);
 	});
 
 	app.post('/api/friends', function(req, res){        
-	   res.json(friendsData);
+	    friendsArray.push(req.body);
+        res.json(true); // KEY LINE
 
 	   //add answer comparison logic.
 	});
+
+	app.post('/api/clear', function(req, res){
+        // Empty out the arrays of data
+        friendsArray = [];
+
+        console.log(friendsArray);
+    })
 
 }
 
