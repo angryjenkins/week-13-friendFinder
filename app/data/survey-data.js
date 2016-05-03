004cbae85b3aac0bca40e3e6b032761d514c5dc4
m. nathaniel martin <matt.martin@rutgers.edu>
2016-05-03 18:23:12 -0400

    AJAX problems!

--- app/data/survey-data.js
+++ app/data/survey-data.js
@@ -2,7 +2,12 @@ var friendsArray = [
 	{
 		name: 'Matt',
 		pic:'https://avatars2.githubusercontent.com/u/14875183?v=3&s=460',
-		score: '35'
+		scores: [1,2,3,1,2,3,1,2,3,1]
+	},
+	{
+		name: 'Buster',
+		pic: 'https://i.ytimg.com/vi/tntOCGkgt98/maxresdefault.jpg',
+		scores: [3,2,1,3,2,1,3,2,1,3]
 	}
 ];
 
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -6,25 +6,52 @@
 var friendsArray = require('../data/survey-data.js');
 var path = require('path');
 
+var comparisons = [];
+
 module.exports = function(app){
 
-	app.get('/api/friends', function(req, res){
-	    res.json(friendsArray);
-	});
+app.get('/api/friends', function(req, res){
+    res.json(friendsArray);
+});
 
-	app.post('/api/friends', function(req, res){        
-	    friendsArray.push(req.body);
-        res.json(true); // KEY LINE
+app.post('/api/friends', function(req, res){        
+        friendsArray.push(req.body);
 
-	   //add answer comparison logic.
-	});
+        console.log("friend finder submitted!"); 
+        console.log(req.body);
 
-	app.post('/api/clear', function(req, res){
-        // Empty out the arrays of data
-        friendsArray = [];
+// res.json(true);
+//add answer comparison logic.
+        var formData = req.body;
 
-        console.log(friendsArray);
-    })
+        for(i=0;i<friendsArray.length;i++){
+                var compatibility=0;
+                var diff=0;
+
+                for (x=0;x<formData.scores.length;x++){
+                      if (formData.scores[x] - friendsArray[i].scores[x]){
+                        compatibility++;
+                      } 
+
+                }
+
+
+                var friend = {
+                        name: friendsArray[i].name,
+                        compatibility: compatibility
+                }
+                console.log('Friend: %s', friend.name);
+                console.log('Compatibility: %s', friend.compatibility);
+                comparisons.push(friend);
+       }
+});
+
+// app.post('/api/clear', function(req, res){
+//        // Empty out the arrays of data
+//        friendsArray = [];
+
+//        console.log(friendsArray);
+//    })
 
 }
 
