d46d775bc7999357867b2206254094ca5e8fbc26
m. nathaniel martin <matt.martin@rutgers.edu>
2016-05-04 00:55:52 -0400

    todo: add compare logic, make modal work.

--- app/data/survey-data.js
+++ app/data/survey-data.js
@@ -3,6 +3,11 @@ var friendsArray = [
 		name: 'Matt',
 		pic:'https://avatars2.githubusercontent.com/u/14875183?v=3&s=460',
 		input: ["Blue","Football","Australia","Shop","Prince","Truck","Axe","Snake","Bullwinkle J. Moose","Monopoly"]
+	},
+	{
+		name: 'Buster',
+		pic:'https://avatars2.githubusercontent.com/u/14875183?v=3&s=460',
+		input: ["Red","Hockey","France","Hunt","Mickey Mantle","Car","Shotgun","Dog","Mickey Mouse","Clue"]
 	}
 ];
 
